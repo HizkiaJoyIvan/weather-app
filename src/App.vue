9cc2f19b1dbfbfa993f3574a3e6786cb6956a642
Zoey <joyivan6@gmail.com>
2023-12-24 18:37:01 +0700

    feat: add more information modal

--- src/App.vue
+++ src/App.vue
@@ -17,6 +17,29 @@
       <div class="text-white opacity-90 font-semibold">{{ dateBuilder() }}</div>
       <div class="bg-white opacity-40 py-5 px-2 text-7xl text-center rounded-lg font-extrabold shadow-md">{{ weather.main.temp }}ºC</div>
       <div class="text-3xl text-white font-bold opacity-90">{{ weather.weather[0].main }}</div>
+      <div 
+        class="text-slate-200 font-semibold hover:cursor-pointer hover:text-white"
+        @click="showMoreInfo"
+      >
+        More information
+      </div>
+    </div>
+    <div v-if="showModal" class="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-75">
+      <div class="bg-white p-8 rounded-lg shadow-md">
+        <div class="flex justify-between">
+          <div>
+            <div class="text-gray-600 font-semibold">
+              Min Temp: {{ weather.main.temp_min }}ºC<br>
+              Max Temp: {{ weather.main.temp_max }}ºC<br>
+              Wind: {{ weather.wind.speed }} m/s<br>
+              Feels like: {{ weather.main.feels_like }}ºC
+            </div>
+          </div>
+        </div>
+        <div class="mt-4 text-right">
+          <button @click="closeMoreInfo" class="text-white bg-cyan-700 hover:bg-cyan-600 p-2 rounded-md font-semibold">Close</button>
+        </div>
+      </div>
     </div>
     <div></div>
   </div>
@@ -30,7 +53,8 @@
         api_key: '87274c464f83246a5bdb4d545994e43f',
         url_base: 'http://api.openweathermap.org/data/2.5/',
         query: '',
-        weather: {}
+        weather: {},
+        showModal: false
       }
     },
     methods: {
@@ -55,6 +79,12 @@
         let month = months[d.getMonth()]
         let year = d.getFullYear()
         return `${day}, ${date} ${month} ${year}`
+      },
+      showMoreInfo() {
+        this.showModal = true
+      },
+      closeMoreInfo() {
+        this.showModal = false
       }
     }
   }
